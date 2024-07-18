fda4a8e51ef0171535d0d46bfdbf821d74e7fca7
HolyMolly <developer.code5417@gmail.com>
2024-07-18 20:30:16 +0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,2 @@
+
+import'./modules/chunks/dynamic_adap.js';import'./modules/chunks/lang.js';import'./modules/chunks/burger.js';import'./modules/chunks/header_scroll.js';import'./modules/scripts.js';
