fa9e8eb6465f55e20774015f3e08ff15c2e7c0b2
HolyMolly <developer.code5417@gmail.com>
2024-07-18 21:27:43 +0300

    Updates

--- js/modules/chunks/burger.js
+++ js/modules/chunks/burger.js
@@ -1,2 +1,24 @@
+import { bodyLock, bodyUnLock } from '../functions/body_lock.js';
 
-import{bodyLock,bodyUnLock}from'../functions/body_lock.js';const body=window.document.body;const headerMain=body.querySelector('.header-main');if(headerMain){const burgerIcon=headerMain.querySelector('[data-menu="toggle"]');const dropdown=headerMain.querySelector('[data-menu="dropdown"]');if(burgerIcon&&dropdown){burgerIcon.addEventListener('click',()=>{burgerIcon.classList.toggle('_active');dropdown.classList.toggle('_active');headerMain.classList.toggle('_active');if(headerMain.classList.contains('_active')){bodyLock();}else{bodyUnLock();}});}}
+const body = window.document.body;
+
+const headerMain = body.querySelector('.header-main');
+
+if (headerMain) {
+  const burgerIcon = headerMain.querySelector('[data-menu="toggle"]');
+  const dropdown = headerMain.querySelector('[data-menu="dropdown"]');
+
+  if (burgerIcon && dropdown) {
+    burgerIcon.addEventListener('click', () => {
+      burgerIcon.classList.toggle('_active');
+      dropdown.classList.toggle('_active');
+      headerMain.classList.toggle('_active');
+
+      if (headerMain.classList.contains('_active')) {
+        bodyLock();
+      } else {
+        bodyUnLock();
+      }
+    });
+  }
+}
--- js/modules/chunks/lang.js
+++ js/modules/chunks/lang.js
@@ -1,2 +1,21 @@
+const body = window.document.body;
 
-const body=window.document.body;const langParent=body.querySelector('[data-lang="parent"]');if(langParent){const toggle=langParent.querySelector('[data-lang="toggle"]');const dropdown=langParent.querySelector('[data-lang="dropdown"]');if(toggle&&dropdown){toggle.addEventListener('click',()=>{dropdown.classList.add('_active');});body.addEventListener('click',(e)=>{const{target}=e;if(dropdown.classList.contains('_active')&&!target.closest('[data-lang="dropdown"], [data-lang="toggle"]')){body.querySelector('[data-lang="dropdown"]').classList.remove('_active');}});}}
+const langParent = body.querySelector('[data-lang="parent"]');
+if (langParent) {
+  const toggle = langParent.querySelector('[data-lang="toggle"]');
+  const dropdown = langParent.querySelector('[data-lang="dropdown"]');
+
+  if (toggle && dropdown) {
+    toggle.addEventListener('click', () => {
+      dropdown.classList.add('_active');
+    });
+
+    body.addEventListener('click', (e) => {
+      const { target } = e;
+
+      if (dropdown.classList.contains('_active') && !target.closest('[data-lang="dropdown"], [data-lang="toggle"]')) {
+        body.querySelector('[data-lang="dropdown"]').classList.remove('_active');
+      }
+    });
+  }
+}
