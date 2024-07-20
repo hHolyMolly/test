d58135a10cd1393cd51afea442e343a508a9a508
HolyMolly <developer.code5417@gmail.com>
2024-07-20 06:00:11 +0300

    Updates

--- js/modules/chunks/exchanger_form.js
+++ js/modules/chunks/exchanger_form.js
@@ -1,29 +1,33 @@
 const body = window.document.body;
 
-const exchangerElements = body.querySelectorAll('[data-exchanger-form="from"], [data-exchanger-form="to"]');
+const form = body.querySelector('[data-exchanger-form="main"]');
 
-if (exchangerElements.length > 0) {
-  exchangerElements.forEach((elem) => {
-    const button = elem.querySelector('[data-exchanger-form="target"]');
-    const dropdown = elem.querySelector('[data-exchanger-form="dropdown"]');
-    const close = elem.querySelector('[data-exchanger-form="close"]');
+if (form) {
+  const exchangerElements = body.querySelectorAll('[data-exchanger-form="from"], [data-exchanger-form="to"]');
 
-    if (button && dropdown && close) {
-      button.addEventListener('click', () => {
-        dropdown.classList.add('_active');
-      });
+  if (exchangerElements.length > 0) {
+    exchangerElements.forEach((elem) => {
+      const button = elem.querySelector('[data-exchanger-form="target"]');
+      const dropdown = elem.querySelector('[data-exchanger-form="dropdown"]');
+      const close = elem.querySelector('[data-exchanger-form="close"]');
 
-      close.addEventListener('click', () => {
-        dropdown.classList.remove('_active');
-      });
+      if (button && dropdown && close) {
+        button.addEventListener('click', () => {
+          dropdown.classList.add('_active');
+        });
 
-      body.addEventListener('click', (e) => {
-        const { target } = e;
-
-        if (!target.closest('[data-exchanger-form="target"]') && !target.closest('[data-exchanger-form="dropdown"]')) {
+        close.addEventListener('click', () => {
           dropdown.classList.remove('_active');
-        }
-      });
-    }
-  });
+        });
+
+        body.addEventListener('click', (e) => {
+          const { target } = e;
+
+          if (!target.closest('[data-exchanger-form="target"]') && !target.closest('[data-exchanger-form="dropdown"]')) {
+            dropdown.classList.remove('_active');
+          }
+        });
+      }
+    });
+  }
 }
